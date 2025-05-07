f8cdb9e0c8e62fcab5fe80d440ad1770ec27a6cc
Davi Damasceno de Sousa <159800322+davitkblade@users.noreply.github.com>
2025-05-07 16:51:45 -0300

    Add files via upload

---
+++ src/components/Calculadora.jsx
@@ -0,0 +1,18 @@
+export default function App (){
+    return (
+        <div id="body">
+        <h1>Calculadora Simples</h1>
+        
+        <input type="number" placeholder="Digite o primeiro número" /> <br />
+        <input type="number" placeholder="Digite o segundo número" /> <br />
+
+            <button> Somar </button> 
+            <button> Subtrair </button> 
+            <button> Multiplicar </button> 
+            <button> Dividir </button> 
+
+            <p>Resultado:</p>
+        </div>
+    )
+}
+
